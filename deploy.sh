3cbe9975e96f29a62e461bcae54e5505e7159128
nkasprak@gmail.com <nkasprak@gmail.com>
2024-12-18 11:42:29 -0500

    change deploy dest

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 rsync -av \
   -e "/usr/bin/ssh" \
-  ./html/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/$PROJECTID/
+  ./html/ cbppapps@vps42437.dreamhostps.com:/home/cbppapps/apps.cbpp.org/$PROJECTID/
 
 rsync -av \
   -e "/usr/bin/ssh" \
-  ./node/prod/ cbppapps@apps.cbpp.org:/home/cbppapps/apps.cbpp.org/$PROJECTID/js/
+  ./node/prod/ cbppapps@vps42437.dreamhostps.com:/home/cbppapps/apps.cbpp.org/$PROJECTID/js/
