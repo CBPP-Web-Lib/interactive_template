9bccee789dc34071b843d40e82e692b1adb44fc2
nkasprak@gmail.com <nkasprak@gmail.com>
2024-12-18 11:53:49 -0500

    add basic selectors/url pattern I use everywhere

--- node/src/app.js
+++ node/src/app.js
@@ -2,4 +2,12 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "./style.scss";
 
+const id = "$PROJECTID"
+const script_id = "script_" + id
+const sel = "#" + id
+const script_sel = "#" + script_id
+
+/*Gets the correct base URL regardless of whether we're on the test apps.cbpp.org page or the real cbpp.org page*/
+const url_base = document.querySelector(script_sel).src.replace(/js\/app(\.min)*\.js/g,"").split("?")[0]
+
 document.write("Hello world!")
