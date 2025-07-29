01e630531dd121a739c0bf86ac1d391e568a22b2
Nick Kasprak <nkasprak@cbpp.org>
2025-07-29 10:05:00 -0400

    don't deploy permissions

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 rsync -av \
   -e "/usr/bin/ssh" \
+  --no-perms \
   ./html/ cbppapps@vps42437.dreamhostps.com:/home/cbppapps/apps.cbpp.org/$PROJECTID/
 
 rsync -av \
   -e "/usr/bin/ssh" \
+  --no-perms \
   ./node/prod/ cbppapps@vps42437.dreamhostps.com:/home/cbppapps/apps.cbpp.org/$PROJECTID/js/
